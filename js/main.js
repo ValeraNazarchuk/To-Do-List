8092582de9406ec026d34da04102fcaacd23144d
ValeraNazarchuk <valera55555nazarchuk@gmail.com>
2023-01-26 17:39:50 +0200

    added the number of tasks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const headerText = document.querySelector('.header__text')
+
 const form = document.querySelector('.form')
 const taskInput = document.querySelector('.form__text')
 const formBtn = document.querySelector('.form__btn')
@@ -13,6 +15,7 @@ if (localStorage.getItem('tasks')) {
 }
 
 checkEmptyList()
+numberTask(tasks.length)
 
 form.addEventListener('submit', addTask)
 
@@ -31,10 +34,14 @@ function addTask(e) {
     done: false,
   }
 
-  tasks.push(newTask)
-
+  // tasks.push(newTask)
+  
   // filter for css class
-  renderTask(newTask)
+  if (taskText) {
+    tasks.push(newTask)
+    renderTask(newTask)
+    numberTask(tasks.length)
+  }
 
   // почистити поле ввода і додати фокус
   taskInput.value = ''
@@ -53,13 +60,11 @@ function deleteTask(e) {
 
   tasks = tasks.filter((task) => task.id !== id)
 
-  // const index = tasks.findIndex( (task) => task.id === id)
-
-  // tasks.splice(index, 1)
   parentNode.remove()
 
   checkEmptyList()
   saveToLocalStorage()
+  numberTask(tasks.length)
 }
 
 function doneTask(e) {
@@ -107,5 +112,9 @@ function renderTask(task) {
           <p class="${cssClass}">${task.text}</p>
         </li>`
 
-  taskList.insertAdjacentHTML('beforeend', taskHTML)
+  taskList.insertAdjacentHTML('afterbegin', taskHTML)
+}
+
+function numberTask(args) {
+  headerText.textContent = `${args} tasks`
 }
